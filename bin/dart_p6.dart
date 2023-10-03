b8017131968aee1190ca766fb3ae1b60fbd59e62
hafizhpebrian <hafizhpebrian_2226250077@mhs.mdp.ac.id>
2023-10-03 12:22:18 +0700

    Initial Project

---
+++ bin/dart_p6.dart
@@ -0,0 +1,5 @@
+import 'package:dart_p6/dart_p6.dart' as dart_p6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_p6.calculate()}!');
+}
---
+++ test/dart_p6_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_p6/dart_p6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
